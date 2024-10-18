790ebffa925a66b8ddbe29d29a425d31b3fce641
k730hs <k730hs@gmail.com>
2024-10-18 14:31:55 +0800

    Change the default roles from Employee to Salesperson

--- client/admin-fe/src/features/users/NewUserForm.js
+++ client/admin-fe/src/features/users/NewUserForm.js
@@ -22,7 +22,7 @@ const NewUserForm = () => {
   const [validUsername, setValidUsername] = useState(false);
   const [password, setPassword] = useState("");
   const [validPassword, setValidPassword] = useState(false);
-  const [roles, setRoles] = useState(["Employee"]);
+  const [roles, setRoles] = useState(["Salesperson"]);
 
   useEffect(() => {
     setValidName(NAME_REGEX.test(name));
