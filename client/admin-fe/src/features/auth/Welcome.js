73d36ff8a5c3d322915794bd433a117c5ba3624b
kY2759tJ <k730hs@gmail.com>
2024-10-18 22:25:55 +0800

    - change text notes to campaigns

--- client/admin-fe/src/features/auth/Welcome.js
+++ client/admin-fe/src/features/auth/Welcome.js
@@ -13,18 +13,19 @@ const Welcome = () => {
       <h1>Welcome!</h1>
 
       <p>
-        <Link to="/dash/campaigns">View techNotes</Link>
-      </p>
-
-      <p>
-        <Link to="/dash/campaigns/new">Add New techNotes</Link>
-      </p>
-      <p>
         <Link to="/dash/users">View User Settings</Link>
       </p>
 
       <p>
         <Link to="/dash/users/new">Add New User Settings</Link>
+      </p>
+
+      <p>
+        <Link to="/dash/campaigns">View Campaigns</Link>
+      </p>
+
+      <p>
+        <Link to="/dash/campaigns/new">Add New Campaign</Link>
       </p>
     </section>
   );
