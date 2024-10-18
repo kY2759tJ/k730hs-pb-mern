9f72e6fd71c1e0415597a0986ca96e3db89757ec
k730hs <k730hs@gmail.com>
2024-10-18 12:03:04 +0800

    added salespersonDetails to userSchema

--- server/models/User.js
+++ server/models/User.js
@@ -23,6 +23,9 @@ const userSchema = new mongoose.Schema({
     type: Boolean,
     default: true,
   },
+  salespersonDetails: {
+    commission_rate: Number,
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
