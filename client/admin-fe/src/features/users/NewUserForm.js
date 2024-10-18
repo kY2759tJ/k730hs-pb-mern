f4ef51025217827493b575d0b22bff71b84463e8
k730hs <k730hs@gmail.com>
2024-10-18 13:13:44 +0800

    - Updated New User Form - Used Ant Design

--- client/admin-fe/src/features/users/NewUserForm.js
+++ client/admin-fe/src/features/users/NewUserForm.js
@@ -4,7 +4,9 @@ import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSave } from "@fortawesome/free-solid-svg-icons";
 import { ROLES } from "../../config/roles";
+import { Form, Input, Button, Select } from "antd";
 
+const NAME_REGEX = /^[a-zA-Z]+([ '-][a-zA-Z]+)*$/;
 const USER_REGEX = /^[A-z]{3,20}$/;
 const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/;
 
@@ -14,11 +16,17 @@ const NewUserForm = () => {
 
   const navigate = useNavigate();
 
+  const [name, setName] = useState("");
+  const [validName, setValidName] = useState(false);
   const [username, setUsername] = useState("");
   const [validUsername, setValidUsername] = useState(false);
   const [password, setPassword] = useState("");
   const [validPassword, setValidPassword] = useState(false);
   const [roles, setRoles] = useState(["Employee"]);
+
+  useEffect(() => {
+    setValidName(NAME_REGEX.test(name));
+  }, [name]);
 
   useEffect(() => {
     setValidUsername(USER_REGEX.test(username));
@@ -30,6 +38,7 @@ const NewUserForm = () => {
 
   useEffect(() => {
     if (isSuccess) {
+      setName("");
       setUsername("");
       setPassword("");
       setRoles([]);
@@ -37,40 +46,34 @@ const NewUserForm = () => {
     }
   }, [isSuccess, navigate]);
 
+  const onNameChanged = (e) => setName(e.target.value);
   const onUsernameChanged = (e) => setUsername(e.target.value);
   const onPasswordChanged = (e) => setPassword(e.target.value);
 
-  const onRolesChanged = (e) => {
-    const values = Array.from(
-      e.target.selectedOptions, //HTMLCollection
-      (option) => option.value
-    );
-    setRoles(values);
+  const onRolesChanged = (selectedRoles) => {
+    setRoles(selectedRoles);
   };
 
   const canSave =
-    [roles.length, validUsername, validPassword].every(Boolean) && !isLoading;
+    [roles.length, validName, validUsername, validPassword].every(Boolean) &&
+    !isLoading;
 
-  const onSaveUserClicked = async (e) => {
-    e.preventDefault();
+  const onSaveUserClicked = async (values) => {
+    console.log(values);
     if (canSave) {
-      await addNewUser({ username, password, roles });
+      await addNewUser({ name, username, password, roles });
     }
   };
 
-  const options = Object.values(ROLES).map((role) => {
-    return (
-      <option
-        key={role}
-        value={role}
-      >
-        {" "}
-        {role}
-      </option>
-    );
-  });
+  const options = Object.values(ROLES).map((role) => ({
+    value: role,
+    label: role,
+  }));
+
+  console.log(options);
 
   const errClass = isError ? "errmsg" : "offscreen";
+  const validNameClass = !validName ? "form__input--incomplete" : "";
   const validUserClass = !validUsername ? "form__input--incomplete" : "";
   const validPwdClass = !validPassword ? "form__input--incomplete" : "";
   const validRolesClass = !Boolean(roles.length)
@@ -80,72 +83,77 @@ const NewUserForm = () => {
   const content = (
     <>
       <p className={errClass}>{error?.data?.message}</p>
-
-      <form
-        className="form"
-        onSubmit={onSaveUserClicked}
+      <Form
+        labelCol={{ span: 4 }}
+        wrapperCol={{ span: 14 }}
+        layout="horizontal"
+        style={{ maxWidth: 600 }}
+        onFinish={onSaveUserClicked}
       >
         <div className="form__title-row">
           <h2>New User</h2>
           <div className="form__action-buttons">
-            <button
-              className="icon-button"
+            <Button
+              type="primary"
               title="Save"
+              htmlType="submit"
               disabled={!canSave}
             >
-              <FontAwesomeIcon icon={faSave} />
-            </button>
+              Submit
+            </Button>
           </div>
         </div>
-        <label
-          className="form__label"
-          htmlFor="username"
+        <Form.Item
+          name="name"
+          label="Name"
+          rules={[{ required: true, message: "Please input your full name!" }]}
+          className={`form__input ${validNameClass}`}
+          value={name}
+          onChange={onNameChanged}
         >
-          Username: <span className="nowrap">[3-20 letters]</span>
-        </label>
-        <input
-          className={`form__input ${validUserClass}`}
-          id="username"
+          <Input placeholder="Full Name" />
+        </Form.Item>
+        <Form.Item
           name="username"
-          type="text"
-          autoComplete="off"
+          label="Username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+          className={`form__input ${validUserClass}`}
           value={username}
           onChange={onUsernameChanged}
-        />
-
-        <label
-          className="form__label"
-          htmlFor="password"
         >
-          Password: <span className="nowrap">[4-12 chars incl. !@#$%]</span>
-        </label>
-        <input
-          className={`form__input ${validPwdClass}`}
-          id="password"
+          <Input placeholder="[3-20 letters]" />
+        </Form.Item>
+        <Form.Item
           name="password"
-          type="password"
+          label="Password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+          className={`form__input ${validPwdClass}`}
           value={password}
           onChange={onPasswordChanged}
-        />
-
-        <label
-          className="form__label"
-          htmlFor="roles"
         >
-          ASSIGNED ROLES:
-        </label>
-        <select
-          id="roles"
+          <Input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={onPasswordChanged}
+          />
+        </Form.Item>
+        <Form.Item
           name="roles"
-          className={`form__select ${validRolesClass}`}
-          multiple={true}
-          size="3"
-          value={roles}
-          onChange={onRolesChanged}
+          label="Roles"
+          rules={[{ required: true, message: "Please select roles!" }]}
         >
-          {options}
-        </select>
-      </form>
+          <Select
+            mode="multiple"
+            placeholder="Select roles"
+            style={{ width: "100%" }}
+            className={`form__select ${validRolesClass}`}
+            value={roles}
+            options={options}
+            onChange={onRolesChanged}
+          ></Select>
+        </Form.Item>
+      </Form>
     </>
   );
 
