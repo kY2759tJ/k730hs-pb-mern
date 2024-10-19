bcbc65aa6caa9430e1f798188565291a8f15b3a7
kY2759tJ <k730hs@gmail.com>
2024-10-19 17:25:37 +0800

    chap8 - Authentication & Authorizaiton

--- server/routes/campaignRoutes.js
+++ server/routes/campaignRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const campaignController = require("../controllers/campaignControllers");
+const verifyJWT = require("../middleware/verifyJWT");
+
+router.use(verifyJWT);
 
 router
   .route("/")
--- server/server.js
+++ server/server.js
@@ -27,6 +27,7 @@ app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "public"))); //tell where to find static file
 
 app.use("/", require("./routes/root"));
+app.use("/auth", require("./routes/authRoutes"));
 app.use("/users", require("./routes/userRoutes"));
 app.use("/campaigns", require("./routes/campaignRoutes"));
 
