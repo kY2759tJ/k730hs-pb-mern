80810912581a07e67c047ebba01e4ed3ffa67da4
k730hs <k730hs@gmail.com>
2024-10-22 14:08:44 +0800

    fix user.js

--- server/models/User.js
+++ server/models/User.js
@@ -3,35 +3,32 @@ const mongoose = require("mongoose");
 // Define valid social media type
 const USERROLES = ["Admin", "Salesperson"];
 
-const userSchema = new mongoose.Schema(
-  {
-    fullname: {
-      type: String,
-      required: true,
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    roles: {
-      type: [String],
-      enum: USERROLES,
-      default: ["Salesperson"],
-    },
-    active: {
-      type: Boolean,
-      default: true,
-    },
-    commissionRate: {
-      type: Number,
-      default: "0",
-    },
+const userSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
   },
-  
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  roles: {
+    type: [String],
+    enum: USERROLES,
+    default: ["Salesperson"],
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
+  commissionRate: {
+    type: Number,
+    default: "0",
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
